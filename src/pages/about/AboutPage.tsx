90ac590a702adb64af83fed18d798e490c74c7a2
Matias Sfer <matias0developer@gmail.com>
2023-04-24 18:05:43 -0300

    writing content

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -12,7 +12,7 @@ import {
 import { useEffect, useRef, useState } from "react";
 import { useInViewport } from "react-in-viewport";
 import { SaitamaCard, MyStack } from "./components";
-import {FcCommandLine, FcLibrary, FcUnlock} from "react-icons/fc"
+import { FcCommandLine, FcLibrary, FcUnlock } from "react-icons/fc";
 import { quotes, authors } from "../../const/quotes";
 import { Flex } from "@chakra-ui/react";
 
@@ -39,9 +39,8 @@ export const AboutPage = () => {
       </Heading>
 
       {/* Cartas a lo largo con caracteristicas mias - card: content / icon */}
-      <Box w='80%' m='auto'>
-        
-      <Card
+      <Box w="80%" m="auto">
+        <Card
           rounded="2xl"
           variant="elevated"
           boxShadow="6px 1px 25px 12px rgba(0,0,0,0.25)"
@@ -49,16 +48,23 @@ export const AboutPage = () => {
         >
           <CardHeader paddingBottom={0}>
             <Heading fontFamily="cursive" size="md">
-              My Values
+              Who I am and what my values are?
             </Heading>
           </CardHeader>
           <CardBody>
             <Flex gap={5}>
               <Text>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Laborum expedita maiores assumenda id voluptatibus distinctio
-                fugit doloremque facilis iste? Aliquid velit voluptas mollitia
-                dolore, ipsa magni doloribus nesciunt obcaecati illo!
+                Throughout my life, I have crossed paths with different people
+                and shared unique and special moments with each of them, thanks
+                to diversity and empathy. By putting myself in their shoes, I
+                realized that I didn't really know my origins or reflect on
+                them. Recently, I started a long journey to rediscover myself
+                and understand what and why I do what I do. I have strong
+                principles and fundamental ideas, but I don't see the full
+                spectrum of my own being. One of the things that govern my
+                actions is the desire to always help others and care for their
+                physical and psychological well-being. I wish for the world to
+                grow spiritually so that we can advance as a society.
               </Text>
               <Icon boxSize={5} as={FcLibrary} />
             </Flex>
@@ -73,16 +79,22 @@ export const AboutPage = () => {
         >
           <CardHeader paddingBottom={0}>
             <Heading fontFamily="cursive" size="md">
-              My Values
+              My vision for the future
             </Heading>
           </CardHeader>
           <CardBody>
             <Flex gap={5}>
               <Text>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Laborum expedita maiores assumenda id voluptatibus distinctio
-                fugit doloremque facilis iste? Aliquid velit voluptas mollitia
-                dolore, ipsa magni doloribus nesciunt obcaecati illo!
+                I believe that just like life itself, the future will be full of
+                problems, and new needs will arise that must be met by different
+                services. Humans are becoming increasingly intertwined with
+                technology, to the point where we embrace it as part of our
+                nature. With this in mind, I want to have knowledge in as many
+                diverse areas as possible, ranging from front-end to back-end
+                development, all the way to Cloud services and Data Management.
+                I aim to achieve a broad range of skills that will allow me to
+                contribute to possible solutions that may emerge in the near
+                future.
               </Text>
               <Icon boxSize={5} as={FcCommandLine} />
             </Flex>
@@ -97,22 +109,26 @@ export const AboutPage = () => {
         >
           <CardHeader paddingBottom={0}>
             <Heading fontFamily="cursive" size="md">
-              My Values
+              My interests and hobbies
             </Heading>
           </CardHeader>
           <CardBody>
             <Flex gap={5}>
               <Text>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Laborum expedita maiores assumenda id voluptatibus distinctio
-                fugit doloremque facilis iste? Aliquid velit voluptas mollitia
-                dolore, ipsa magni doloribus nesciunt obcaecati illo!
+                I am a very active and extremely curious person, and these two
+                qualities have shaped me throughout my life. I have acquired
+                general knowledge about ancient and modern history, medicine,
+                biology, and psychology. My studies have led me to become
+                interested in physics and mainly in programming, which has
+                become one of my greatest passions along with soccer and
+                physical training. I am constantly learning something new or
+                engaging in physical activity, and I also enjoy playing online
+                with friends and having a good time. For the Horde!
               </Text>
               <Icon boxSize={5} as={FcUnlock} />
             </Flex>
           </CardBody>
         </Card>
-
       </Box>
       <ScaleFade initialScale={0.9} in={enterCount > 0}>
         <Box
