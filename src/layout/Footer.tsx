61784b8c4f9743d5788bda26f639381bef8c3434
Matias Sfer <matias0developer@gmail.com>
2023-05-04 03:01:34 -0300

    links added into footer icons

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,52 +1,65 @@
 import { FaInstagram, FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
-import { Flex, IconButton} from "@chakra-ui/react";
-import { useState, useEffect } from 'react';
-
+import { Flex, IconButton, Link } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
 export const Footer = () => {
-  const [footerHide, setshowingFooter] = useState(true)
+  const [footerHide, setshowingFooter] = useState(true);
   useEffect(() => {
     setTimeout(() => {
       setshowingFooter(false);
-    }, 1500)
-  
-  }, [])
-  
+    }, 1500);
+  }, []);
+
   return (
-    <Flex justifyContent="center" alignItems="center" py={4} mt="30px" mb="40px" hidden={footerHide}>
-      <IconButton
-        aria-label="Instagram"
-        icon={<FaInstagram />}
-        variant="ghost"
-        color="#BF4F45"
-        backgroundColor='#987cb'
-        size="lg"
-        rounded='lg'
-        mr={4}
-      />
-      <IconButton
-        aria-label="LinkedIn"
-        icon={<FaLinkedin />}
-        variant="ghost"
-        colorScheme="blue"
-        size="lg"
-        mr={4}
-      />
-      <IconButton
-        aria-label="GitHub"
-        icon={<FaGithub />}
-        variant="ghost"
-        color="#465973"
-        size="lg"
-        mr={4}
-      />
-      <IconButton
-        aria-label="Mail"
-        icon={<FaEnvelope />}
-        variant="ghost"
-        color="#BF4F45"
-        size="lg"
-      />
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      py={4}
+      mt="30px"
+      mb="40px"
+      hidden={footerHide}
+    >
+      <Link target="__blank" href="">
+        <IconButton
+          aria-label="Instagram"
+          icon={<FaInstagram />}
+          variant="ghost"
+          color="#BF4F45"
+          backgroundColor="#987cb"
+          size="lg"
+          rounded="lg"
+          mr={4}
+        />
+      </Link>
+      <Link target="__blank" href="https://www.linkedin.com/in/sfer-matias/">
+        <IconButton
+          aria-label="LinkedIn"
+          icon={<FaLinkedin />}
+          variant="ghost"
+          colorScheme="blue"
+          size="lg"
+          mr={4}
+        />
+      </Link>
+      <Link target="__blank" href="https://github.com/matiassf1">
+        <IconButton
+          aria-label="GitHub"
+          icon={<FaGithub />}
+          variant="ghost"
+          color="#465973"
+          size="lg"
+          mr={4}
+        />
+      </Link>{" "}
+      <Link target="__blank" href="">
+        <IconButton
+          aria-label="Mail"
+          icon={<FaEnvelope />}
+          variant="ghost"
+          color="#BF4F45"
+          size="lg"
+        />
+      </Link>
     </Flex>
   );
 };
